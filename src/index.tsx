31dc6cddf216ef9dcbbf83b701aaaa2ff4f05599
artakpoghosyan <artakpoghosyan@workfront.com>
2020-05-20 22:11:43 +0400

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import "./view/style/main.scss";
+
+const App = () => {
+    return <div>dddd</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
