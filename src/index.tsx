f1c548892b8a6e9ccf400c839fe6e3f974645cd7
artakpoghosyan <artakpoghosyan@workfront.com>
2020-05-20 22:55:11 +0400

    chore: configuration setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,9 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import "./view/style/main.scss";
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import './view/style/main.scss'
 
 const App = () => {
-    const x=1;
-    console.log(x); function f() {
-        return null
-    }
-    return <div>
-        <span>
-            asas
-            </span>
-    </div>;
-};
+  return <div>dddd</div>
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
