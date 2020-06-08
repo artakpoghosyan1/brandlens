c61eabfc3f59e34b97345544c574cd0d262b54f3
WooFL <wonderfou2@gmail.com>
2020-06-08 05:29:13 +0400

    chore: add new effect with audio reactivity

--- src/scenes/scene1/step1.ts
+++ src/scenes/scene1/step1.ts
@@ -10,11 +10,12 @@ import { Effect } from '@babylonjs/core/Materials/effect'
 import { ShaderMaterial } from '@babylonjs/core/Materials/shaderMaterial'
 import { DynamicTexture } from '@babylonjs/core/Materials/Textures/dynamicTexture'
 import { VideoTexture } from '@babylonjs/core/Materials/Textures/videoTexture'
-import { Color4, Vector3 } from '@babylonjs/core/Maths/math'
+import { Color4, Vector3, Plane } from '@babylonjs/core/Maths/math'
 import { Scene } from '@babylonjs/core/scene'
 import { Sound } from '@babylonjs/core/Audio/sound'
 import { Tools } from '@babylonjs/core/Misc/tools'
 import musicFile from '../../view/assets/images/music_1.mp3'
+import overlayVideo from '../../view/assets/images/overlayVideo.mp4'
 
 export function step_1(engine, offsetWidth, offsetHeight) {
     Effect.ShadersStore['customVertexShader'] =
@@ -32,33 +33,25 @@ export function step_1(engine, offsetWidth, offsetHeight) {
         '    vUV = uv;\r\n' +
         '}\r\n'
 
-    Effect.ShadersStore['customFragmentShader'] =
-        '\r\n' +
-        'precision highp float;\r\n' +
-        'varying vec2 vUV;\r\n' +
-        'uniform sampler2D textureSampler;\r\n' +
-        'uniform float time;\r\n' +
-        'float band(float t, float start, float end, float blur){\r\n' +
-        '   float step1 = smoothstep(start - blur, start + blur, t);\r\n' +
-        '   float step2 = smoothstep(end + blur, end - blur, t);\r\n' +
-        '   return min(step1, step2);\r\n' +
-        '}\r\n' +
-        'void main(void) {\r\n' +
-        '    float num = 2.0;\r\n' +
-        '    vec2 uv = vUV;\r\n' +
-        '   float offset = 1./num;\r\n' +
-        '   float crop = 0.2;\r\n' +
-        '   float feather = 0.1;\r\n' +
-        '   vec4 color = vec4(0);\r\n' +
-        '   for (float i = 0.; i < num; i ++) {\r\n' +
-        '       float xOffset = fract(uv.x - i * offset + crop + time);\r\n' +
-        '       float bleed = band(xOffset, crop, 1. - crop, feather);\r\n' +
-        '       color += texture(textureSampler, vec2(xOffset,uv.y)) * bleed;\r\n' +
-        '   }\r\n' +
-        '   color.rgb = pow(mix(color.rgb, vec3(dot(color.rgb, vec3(0.2125, 0.7154, 0.0721))), vec3(0.8)), vec3(1.3));\r\n' +
-        '   color.a = 1.;\r\n' +
-        '   gl_FragColor = color;\r\n' +
-        '}\r\n'
+    Effect.ShadersStore['customFragmentShader'] = `precision highp float;
+        varying vec2 vUV;
+        uniform sampler2D textureSampler;
+        uniform float time;
+        uniform float offset;
+        uniform float edge;
+
+        float phaser(float pct, float phase, float e) {
+            return clamp( (phase-1.+pct*(1.+e))/e, 0., 1.);
+        }
+
+        void main(void) {
+            vec2 uv = vUV;
+            vec2 uvR = ((uv * 2.0 - 1.0) / vec2(phaser(time, offset * 1.0 / 3.0, edge) + 1.0)) * 0.5 + 0.5;
+            vec2 uvG = ((uv * 2.0 - 1.0) / vec2(phaser(time, offset * 2.0 / 3.0, edge) + 1.0)) * 0.5 + 0.5;
+            vec2 uvB = ((uv * 2.0 - 1.0) / vec2(phaser(time, offset, edge) + 1.0)) * 0.5 + 0.5;
+            vec4 color = vec4(texture(textureSampler, uvR).r,texture(textureSampler, uvG).g,texture(textureSampler, uvB).b, 1.0);
+            gl_FragColor = color;
+        }`
 
     Effect.ShadersStore['overlayVertexShader'] =
         '\r\n' +
@@ -75,16 +68,20 @@ export function step_1(engine, offsetWidth, offsetHeight) {
         '    vUV = uv;\r\n' +
         '}\r\n'
 
-    Effect.ShadersStore['overlayFragmentShader'] =
-        '\r\n' +
-        'precision highp float;\r\n' +
-        'varying vec2 vUV;\r\n' +
-        'uniform sampler2D textureSampler;\r\n' +
-        'void main(void) {\r\n' +
-        '    vec2 uv = vUV;\r\n' +
-        '   vec4 color = texture(textureSampler, uv);\r\n' +
-        '   gl_FragColor = color * color.a + vec4(0.1);\r\n' +
-        '}\r\n'
+    Effect.ShadersStore['overlayFragmentShader'] = `precision highp float;
+
+        varying vec2 vUV;
+        
+        uniform sampler2D textureSampler;
+        
+        void main(void) {
+            vec2 uv = vUV;
+            uv.y *= 0.5;
+            float alpha = texture(textureSampler, uv).r;
+            uv.y += 0.5;
+            vec3 color = texture(textureSampler, uv).rgb;
+            gl_FragColor = vec4(color,alpha);
+        }`
 
     let myVideo
     let isAssigned = false
@@ -104,134 +101,15 @@ export function step_1(engine, offsetWidth, offsetHeight) {
     plane1.rotation.z = Math.PI
     plane1.position.z = -2.35
     plane1.rotation.y = Math.PI
+    plane1.scaling.x = 1.777778
     camera.setTarget(Vector3.Zero())
 
     let plane2 = Mesh.CreatePlane('plane2', 2, scene)
-    plane2.rotation.x = Math.PI
     plane2.position.z = -2.0
     plane2.rotation.y = Math.PI
 
-    let plane3 = Mesh.CreatePlane('plane3', 2, scene)
-    plane3.rotation.x = Math.PI
-    plane3.position.z = -2.0
-    plane3.rotation.y = Math.PI
-
-    let plane4 = Mesh.CreatePlane('plane4', 2, scene)
-    plane3.rotation.x = Math.PI
-    plane3.position.z = -2.0
-    plane3.rotation.y = Math.PI
-
-    let xSlideCool = new Animation(
-        'xSlideCool',
-        'position.x',
-        60,
-        Animation.ANIMATIONTYPE_FLOAT,
-        Animation.ANIMATIONLOOPMODE_CONSTANT
-    )
-
-    let keyFramesCool: { frame: number; value: number }[] = []
-
-    keyFramesCool.push({
-        frame: 0,
-        value: -4,
-    })
-
-    keyFramesCool.push({
-        frame: 15,
-        value: 0,
-    })
-
-    keyFramesCool.push({
-        frame: 25,
-        value: 0,
-    })
-
-    keyFramesCool.push({
-        frame: 40,
-        value: 4,
-    })
-
-    let easingFunction = new CircleEase()
-    easingFunction.setEasingMode(EasingFunction.EASINGMODE_EASEINOUT)
-    xSlideCool.setEasingFunction(easingFunction)
-    xSlideCool.setKeys(keyFramesCool)
-
-    // plane2.animations = [];
-    // plane2.animations.push(xSlideCool);
-
-    let xSlideWithIt = new Animation(
-        'xSlideWithIt',
-        'position.x',
-        60,
-        Animation.ANIMATIONTYPE_FLOAT,
-        Animation.ANIMATIONLOOPMODE_CONSTANT
-    )
-    let keyFramesWithIt: { frame: number; value: number }[] = []
-
-    keyFramesWithIt.push({
-        frame: 0,
-        value: 4,
-    })
-
-    keyFramesWithIt.push({
-        frame: 15,
-        value: 0,
-    })
-
-    keyFramesWithIt.push({
-        frame: 25,
-        value: 0,
-    })
-
-    keyFramesWithIt.push({
-        frame: 40,
-        value: -4,
-    })
-    xSlideWithIt.setKeys(keyFramesWithIt)
-    xSlideWithIt.setEasingFunction(easingFunction)
-    // plane3.animations = [];
-    // plane3.animations.push(xSlideWithIt);
-
-    let xSlideCrew = new Animation(
-        'xSlideCrew',
-        'position.x',
-        60,
-        Animation.ANIMATIONTYPE_FLOAT,
-        Animation.ANIMATIONLOOPMODE_CONSTANT
-    )
-    let keyFramesCrew: { frame: number; value: number }[] = []
-
-    keyFramesCrew.push({
-        frame: 0,
-        value: 4,
-    })
-
-    keyFramesCrew.push({
-        frame: 15,
-        value: 0,
-    })
-
-    keyFramesCrew.push({
-        frame: 25,
-        value: 0,
-    })
-
-    keyFramesCrew.push({
-        frame: 40,
-        value: -4,
-    })
-    xSlideCrew.setKeys(keyFramesCrew)
-    xSlideCrew.setEasingFunction(easingFunction)
-
     const font = 'bold 150px monospace'
-    let textTexture1 = new DynamicTexture('text texture 1', { width: 512, height: 512 }, scene, true)
-    textTexture1.drawText('Sick', 100, 300, font, 'white', 'black', false, true)
-    let textTexture2 = new DynamicTexture('text texture 2', { width: 512, height: 512 }, scene, true)
-    textTexture2.drawText('With it', 100, 300, font, 'white', 'black', false, true)
-    let textTexture3 = new DynamicTexture('text texture 3', { width: 512, height: 512 }, scene, true)
-    textTexture3.drawText('Crew', 100, 300, font, 'white', 'black', false, true)
-    let textTexture4 = new DynamicTexture('text texture 4', { width: 512, height: 512 }, scene, true)
-    textTexture4.drawText('Drop', 100, 300, font, 'white', 'black', false, true)
+    let textTexture1 = new VideoTexture('overlay video', overlayVideo, scene, true)
 
     let effectMaterial = new ShaderMaterial(
         'shader',
@@ -259,16 +137,18 @@ export function step_1(engine, offsetWidth, offsetHeight) {
         }
     )
 
-    let time = 1.0
-    time.toPrecision(5)
-    setInterval(() => {
-        time += 0.001
-        effectMaterial.setFloat('time', +time.toPrecision(5))
-    }, 1)
+    // let time = 1.0
+    // time.toPrecision(5)
+    // setInterval(() => {
+    //     time += 0.001
+    //     effectMaterial.setFloat('time', +time.toPrecision(5))
+    // }, 1)
 
     effectMaterial.backFaceCulling = false
-    effectMaterial.setFloat('time', time)
+    effectMaterial.alphaMode = BABYLON.Engine.ALPHA_ADD
     overlayMaterial.backFaceCulling = false
+    overlayMaterial.alpha = 0.9999
+    overlayMaterial.alphaMode = BABYLON.Engine.ALPHA_ADD
 
     // Create our video texture
     VideoTexture.CreateFromWebCam(
@@ -293,21 +173,16 @@ export function step_1(engine, offsetWidth, offsetHeight) {
         music.play()
     })
 
-    // scene.sounds = [music]
-
-    let audioContext = BABYLON.Engine.audioEngine.audioContext
-    let myAnalyser = new Analyser(scene) //BABYLON.Engine.audioEngine.audioContext?.createAnalyser() //
+    let myAnalyser = new Analyser(scene)
     let audioDestination = BABYLON.Engine.audioEngine.audioContext?.destination
     myAnalyser!.FFT_SIZE = 64
     myAnalyser!.SMOOTHING = 0.5
     let biquadFilter = BABYLON.Engine.audioEngine.audioContext?.createBiquadFilter()
     biquadFilter!.type = 'lowpass'
     biquadFilter!.frequency.value = 180
-    // biquadFilter!.gain.setValueAtTime(25, audioContext!.currentTime);
     myAnalyser.connectAudioNodes(biquadFilter!, audioDestination!)
     // @ts-ignore
     BABYLON.Engine.audioEngine.connectToAnalyser(myAnalyser)
-    // myAnalyser?.connect(audioDestination)
 
     let smoothstep = function (min, max, value) {
         var x = Math.max(0, Math.min(1, (value - min) / (max - min)))
@@ -321,6 +196,40 @@ export function step_1(engine, offsetWidth, offsetHeight) {
         return Math.sqrt(Mean)
     }
 
+    let gaussianKernel1d = (function () {
+        const sqr2pi = Math.sqrt(2 * Math.PI)
+
+        return function gaussianKernel1d(size: number, sigma: number) {
+            let width = (size / 2) | 0
+            let kernel = new Float32Array(width * 2 + 1)
+            let norm = 1.0 / (sqr2pi * sigma)
+            let coefficient = 2 * sigma * sigma
+            let total = 0
+            let x = 0
+
+            for (x = -width; x <= width; x++) {
+                total += kernel[width + x] = norm * Math.exp((-x * x) / coefficient)
+            }
+
+            for (x = 0; x < kernel.length; x++) {
+                kernel[x] /= total
+            }
+
+            return kernel
+        }
+    })()
+
+    let gaussianFilter = function (value: number, size: number, sigma: number) {
+        let filteredValue = 0
+        let dataArray: number[] = []
+        let kernel = gaussianKernel1d(size, sigma)
+        for (let i = 0; i < 10; i++) {
+            filteredValue += value * kernel[i]
+        }
+        return filteredValue
+        // console.log(filteredValue)
+    }
+
     let audioRms: Float32Array = new Float32Array(myAnalyser!.FFT_SIZE / 2)
 
     scene.registerBeforeRender(function () {
@@ -328,27 +237,18 @@ export function step_1(engine, offsetWidth, offsetHeight) {
         for (let i = 0; i < myAnalyser!.getFrequencyBinCount(); i++) {
             audioRms[i] = frequencyData[i] / 255
         }
-        // console.log(CalculateRMS(audioRms),audioRms)
-        plane2.position.x = CalculateRMS(audioRms) * 2
-        // console.log(plane2.position.x)
+        effectMaterial.setFloat('time', CalculateRMS(audioRms))
+        effectMaterial.setFloat('offset', 0.5)
+        effectMaterial.setFloat('edge', 5.0)
+        // plane2.position.x = CalculateRMS(audioRms) * 2
     })
 
     scene.onBeforeRenderObservable.add(function () {
         if (myVideo !== undefined && !isAssigned) {
             if (myVideo.video.readyState == 4) {
                 plane1.material = effectMaterial
-                effectMaterial.setTexture('textureSampler', textTexture1)
                 plane2.material = overlayMaterial
-                overlayMaterial.setTexture('textureSampler', textTexture2)
-                // plane3.material = overlayMaterial
-                // plane2.material = overlayMaterial
-                // let animation1 = scene.beginAnimation(plane2, 0, 100, true)
-                // animation1.onAnimationEnd = () => {
-                //     scene.beginAnimation(plane3, 0, 100, true)
-                //     console.log('second animation started')
-                // }
-                // scene.beginDirectAnimation(plane2, [xSlideCool], 0, 50)
-                // scene.beginDirectAnimation(plane3, [xSlideWithIt], 50, 100)
+                overlayMaterial.setTexture('textureSampler', textTexture1)
 
                 isAssigned = true
             }
